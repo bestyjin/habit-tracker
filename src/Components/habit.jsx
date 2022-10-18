9e6c640f9cc638fc0a0d6b69461a08c2b83cb102
bestyjin <bestyjin0604@gmail.com>
2022-10-18 14:30:30 +0900

    onClick functions with spread operator

--- src/Components/habit.jsx
+++ src/Components/habit.jsx
@@ -1,37 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Habit extends Component {
-    state = {
-        count : 0
-    }
+  handleIncrement = () => {
+    this.props.onIncrement(this.props.habit);
+  };
 
-    handleIncrement = () => {
-        this.setState({count : this.state.count + 1});
-    }
+  handleDecrement = () => {
+    this.props.onDecrement(this.props.habit);
+  };
 
-    handleDecrement = () => {
-        const count = this.state.count - 1;
-        this.setState({count : count < 0 ? 0 : count});
-    }
-    render() {
-        const {name,count} = this.props.habit;
-        console.log(name,count)
-        return (
-            <li className='habit'>
-                <span className='habit-name'>{name}</span>
-                <span className='habit-count'>{count}</span>
-                <button className='habit-button habit-increase' onClick={this.handleIncrement}>
-                    <i className="fa-solid fa-square-plus"></i>
-                </button>
-                <button className='habit-button habit-decrease' onClick={this.handleDecrement}>
-                    <i className="fa-solid fa-square-minus"></i>
-                </button>
-                <button className='habit-button habit-delete'>
-                    <i className="fa-solid fa-trash"></i>
-                </button>
-            </li>
-        );
-    }
+  handleDelete = () => {
+    this.props.onDelete(this.props.habit);
+  };
+
+  render() {
+    const { name, count } = this.props.habit;
+    // console.log(this.props.onIncrement);
+    return (
+      <li className="habit">
+        <span className="habit-name">{name}</span>
+        <span className="habit-count">{count}</span>
+        <button
+          className="habit-button habit-increase"
+          onClick={this.handleIncrement}
+        >
+          <i className="fa-solid fa-square-plus"></i>
+        </button>
+        <button
+          className="habit-button habit-decrease"
+          onClick={this.handleDecrement}
+        >
+          <i className="fa-solid fa-square-minus"></i>
+        </button>
+        <button
+          className="habit-button habit-delete"
+          onClick={this.handleDelete}
+        >
+          <i className="fa-solid fa-trash"></i>
+        </button>
+      </li>
+    );
+  }
 }
 
 export default Habit;
